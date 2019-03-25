95761914bacba9912d619bc6e94a6842e5a6edf2
Erik Demaine <edemaine@mit.edu>
2019-03-25 14:04:35 -0400

    Run gulp

--- debug/spell-checker.js
+++ debug/spell-checker.js
@@ -2997,7 +2997,7 @@ function CodeMirrorSpellChecker(options) {
 		if(!CodeMirrorSpellChecker.aff_loading) {
 			CodeMirrorSpellChecker.aff_loading = true;
 			var xhr_aff = new XMLHttpRequest();
-			xhr_aff.open("GET", "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.aff", true);
+			xhr_aff.open("GET", "https://cdn.jsdelivr.net/npm/hunspell-dict-en-us@0.1.0/en-us.aff", true);
 			xhr_aff.onload = function() {
 				if(xhr_aff.readyState === 4 && xhr_aff.status === 200) {
 					CodeMirrorSpellChecker.aff_data = xhr_aff.responseText;
@@ -3016,7 +3016,7 @@ function CodeMirrorSpellChecker(options) {
 		if(!CodeMirrorSpellChecker.dic_loading) {
 			CodeMirrorSpellChecker.dic_loading = true;
 			var xhr_dic = new XMLHttpRequest();
-			xhr_dic.open("GET", "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.dic", true);
+			xhr_dic.open("GET", "https://cdn.jsdelivr.net/npm/hunspell-dict-en-us@0.1.0/en-us.dic", true);
 			xhr_dic.onload = function() {
 				if(xhr_dic.readyState === 4 && xhr_dic.status === 200) {
 					CodeMirrorSpellChecker.dic_data = xhr_dic.responseText;
@@ -3036,8 +3036,8 @@ function CodeMirrorSpellChecker(options) {
 		// Define what separates a word
 		var rx_word = /^[^!"#$%&()*+,\-./:;<=>?@[\\\]^_`{|}~\s]+/;
 
-		// Ignore words that are just numbers
-		var rx_ignore = /^[0-9]+$/;
+		// Ignore words that are just numbers, and 27D (dimensions)
+		var rx_ignore = /^[0-9]+D?$/;
 
 
 		// Get array of custom words
